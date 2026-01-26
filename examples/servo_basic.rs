8ea82ac8a5c624f0695657e7872d5a7dc3624353
Carl Kadie <carlk@msn.com>
2026-01-25 18:51:39 -0800

    update the demos

--- examples/servo_basic.rs
+++ examples/servo_basic.rs
@@ -23,11 +23,11 @@ async fn inner_main(_spawner: Spawner) -> Result<Infallible> {
         slice: p.PWM_SLICE5,
     };
 
-    servo.set_degrees(45);                          // Move to 45 degrees and hold.
-    Timer::after(Duration::from_secs(1)).await;     // Give servo reasonable time to reach position
-    servo.set_degrees(90);                          // Move to 90 degrees and hold.
-    Timer::after(Duration::from_secs(1)).await;     // Give servo reasonable time to reach position
-    servo.relax();                                  // Let the servo relax. It will re-enable on next set_degrees()
+    servo.set_degrees(45); // Move to 45 degrees and hold.
+    Timer::after(Duration::from_secs(1)).await; // Give servo reasonable time to reach position
+    servo.set_degrees(90); // Move to 90 degrees and hold.
+    Timer::after(Duration::from_secs(1)).await; // Give servo reasonable time to reach position
+    servo.relax(); // Let the servo relax. It will re-enable on next set_degrees()
 
     core::future::pending().await
 }

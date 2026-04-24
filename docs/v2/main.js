aae2bdcac3d78e610c5baedba0c0f6dc7998cec4
Carl Kadie <carlk@msn.com>
2026-04-24 09:38:47 -0700

    Update remote control layout and increment WASM module version

--- docs/v2/main.js
+++ docs/v2/main.js
@@ -65,7 +65,7 @@ for (const button of document.querySelectorAll("button[data-key]")) {
 
 try {
   status.textContent = "loading WASM module";
-  const wasmModule = await import("./pkg/device_envoy_conway_wasm.js");
+  const wasmModule = await import("./pkg/device_envoy_conway_wasm.js?v=2");
   await wasmModule.default();
   conway = new wasmModule.ConwayWeb();
   status.textContent = "ready";
